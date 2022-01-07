b89d88191feb2059e019466a92675276377362a5
Carlo Pane <cpane@outlook.com>
2022-01-07 16:40:34 +1100

    finished blog tutorials

---
+++ src/pages/blog/index.js
@@ -0,0 +1,48 @@
+import * as React from 'react'
+import { graphql, Link } from 'gatsby'
+import Layout from '../../components/layout'
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <ul>
+      {
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+          </article>
+        ))
+      }
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+query MyQuery {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        slug
+        parent {
+          ... on File {
+            id
+            name
+            modifiedTime(formatString: "MMMM D, YYYY")
+          }
+        }
+      }
+    }
+  }
+`
+
+export default BlogPage
