05dd7ad141a926e6da87f93f10c6a837261e26e7
Carlo Pane <cpane@outlook.com>
2024-08-20 23:17:29 +1000

    fix: alter 2

--- next.config.mjs
+++ next.config.mjs
@@ -14,15 +14,6 @@ const nextConfig = {
    */
   basePath: "/carlo-p-tech.github.io",
 
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
